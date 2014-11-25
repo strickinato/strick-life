5245a519bc7c67e35f3eb322e892aa3a362d8c04
Aaron Strick <aaronstrick@gmail.com>
2014-11-25 11:20:14 -0800

    Added POSTS INDEX model, controller, json in API LAYER

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.assets.compile = true
-config.serve_static_assets = true
-config.log_level = :debug
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.assets.compile = true
+  config.serve_static_assets = true
+  config.log_level = :debug
+  
 end

4d0f1442da82d1935d2dd4c11a651cfee2165a50
Aaron Strick <aaronstrick@gmail.com>
2014-11-26 10:42:46 -0800

    Posts are now displayed in descending order by date

--- app/assets/javascripts/collections/posts.js
+++ app/assets/javascripts/collections/posts.js
@@ -1,4 +1,7 @@
 StrickLife.Collections.Posts = Backbone.Collection.extend({
   url:"api/posts",
   model: StrickLife.Models.Post,
+  comparator: function(model) {
+    return model.get("post_date") * -1
+  }
 });

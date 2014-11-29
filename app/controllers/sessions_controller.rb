051c86b5f61dbfa8f71d5a6020903c09095926f4
Aaron Strick <aaronstrick@gmail.com>
2014-11-29 15:48:10 -0800

    Added New Session Form and Logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,30 @@
-# class SessionsController < ApplicationController
-#   def create
-#     email = user_params[:email]
-#     password = user_params[:password]
-#
-#     user = User.find_by_credentials(email, password)
-#
-#     if user
-#       login!(user)
-#       redirect_to root_url
-#     else
-#       flash.now[:errors] = "Invalid Email Or Password"
-#       render :new
-#     end
-#   end
-#
-#   def new
-#     @user = User.new
-#     render :new
-#   end
-#
-#   def destroy
-#   end
-#
-#   private
-#   def user_params
-#     params.require(:user).permit(:password, :email)
-#   end
-# end
+class SessionsController < ApplicationController
+  def create
+    email = user_params[:email]
+    password = user_params[:password]
+    user = User.find_by_credentials(email, password)
+
+    if user
+      login!(user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = ["Invalid Email Or Password"]
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def destroy
+    logout
+    redirect_to new_session_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :email)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,6 @@ class UsersController < ApplicationController
     render :new
   end
 
-  def destroy
-  end
-
   private
   def user_params
     params.require(:user).permit(:password, :email)

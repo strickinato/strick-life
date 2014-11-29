8089ec7df800364fc5beeb12332e45bf0439e9fd
Aaron Strick <aaronstrick@gmail.com>
2014-11-29 15:44:23 -0800

    Added User Login Form and Logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    user.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def logout
+    user.reset_session_token!
+    session[:session_token] = nil;
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
 
+  resources :users, only:[:create, :new]
+
   namespace :api, defaults: {format: :json} do
     resources :posts do
       resources :tags, only: [:index]

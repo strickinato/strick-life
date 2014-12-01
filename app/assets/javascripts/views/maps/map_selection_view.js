17bcacc9df0a064526bbc411acb9b649fb8fea57
Aaron Strick <aaronstrick@gmail.com>
2014-12-01 11:54:26 -0800

    Getting current location to laod onto page

--- app/assets/javascripts/views/maps/map_selection_view.js
+++ app/assets/javascripts/views/maps/map_selection_view.js
@@ -1,5 +1,6 @@
 StrickLife.Views.MapSelectionView = Backbone.View.extend({
   initialize: function(options) {
+    this.getLocationData();
   },
 
   template: JST["maps/map_selection"],
@@ -14,22 +15,38 @@ StrickLife.Views.MapSelectionView = Backbone.View.extend({
     return this;
   },
 
-  createPopover: function() {
-    $('#map-selection-icon').editable({
-      type:'text',
-      title: 'Enter Location'
-    })
-  },
+  // createPopover: function() {
+  //   $('#map-selection-icon').editable({
+  //     type:'text',
+  //     title: 'Enter Location'
+  //   })
+  // },
 
   createAutocomplete: function() {
     var input = document.getElementById("map-search-box")
-    debugger
     var autocomplete = new google.maps.places.Autocomplete(input);
 
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
       alert('yay');
     });
-  }
+  },
+
+  getLocationData: function() {
+    var x = document.getElementById("current-location-view");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showPosition);
+    } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  },
+
+  showPosition: function(position) {
+    alert("Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude);
+
+    $("#current-location-view").html("Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude);
+  },
 
 
 

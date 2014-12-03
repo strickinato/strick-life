e2327ed578349aa88c6a7fe7bce07001b4d25c6b
Aaron Strick <aaronstrick@gmail.com>
2014-12-03 00:08:54 -0800

    Parse incoming payload to create tag collection

--- app/assets/javascripts/models/post.js
+++ app/assets/javascripts/models/post.js
@@ -1,3 +1,19 @@
 StrickLife.Models.Post = Backbone.Model.extend({
-  urlRoot: "api/posts"
+  urlRoot: "api/posts",
+
+  tags: function() {
+    if (!this._tags) {
+      this._tags = new StrickLife.Collections.Tags([], { post_id: this.id });
+    }
+    return this._tags
+  },
+
+  parse: function(payload) {
+    if (payload.tags) {
+      this.tags().set(payload.tags, {parse: true});
+      delete(payload.tags);
+    }
+
+    return payload;
+  }
 });

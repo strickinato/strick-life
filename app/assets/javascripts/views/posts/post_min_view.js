cc421116b2c029fa1fb566277dd1c22336b04b6f
Aaron Strick <aaronstrick@gmail.com>
2014-12-02 23:17:03 -0800

    Truncate minimized posts better

--- app/assets/javascripts/views/posts/post_min_view.js
+++ app/assets/javascripts/views/posts/post_min_view.js
@@ -1,7 +1,14 @@
 StrickLife.Views.PostMinView = Backbone.View.extend({
   initialize: function(){
-    this.shortenedBody = this.model.escape("body").slice(0,140)
     this.listenTo(this.model, "sync", this.render)
+  },
+
+  shortenText: function(){
+    if( this.model.get("body").length > 160 ) {
+      return this.model.escape("body").slice(0,160) + "..."
+    } else {
+      return this.model.escape("body");
+    }
   },
 
   template: JST["posts/post_min"],
@@ -9,9 +16,10 @@ StrickLife.Views.PostMinView = Backbone.View.extend({
   className: "post-min",
 
   render: function(){
+    this.text = this.shortenText()
     var content = this.template({
       post: this.model,
-      text: this.shortenedBody
+      text: this.text
     });
     this.$el.html(content)
 

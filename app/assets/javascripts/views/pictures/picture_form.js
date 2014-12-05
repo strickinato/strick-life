563282402eb7bf9043c20d8f08e8c22d967c8767
Aaron Strick <aaronstrick@gmail.com>
2014-12-04 18:40:53 -0800

    Created picture form skeleton

---
+++ app/assets/javascripts/views/pictures/picture_form.js
@@ -0,0 +1,45 @@
+StrickLife.Views.PictureSelectionView = Backbone.View.extend({
+  initialize: function(options) {
+
+  },
+
+  template: JST["pictures/picture_selection"],
+
+  render: function(){
+    var content = this.template();
+    this.$el.html(content);
+
+    return this;
+  },
+
+  className: "nav-form-item",
+
+  events: {
+    "click #map-selection-icon" : "togglePopover",
+  },
+
+  togglePopover: function(event){
+    var open = $(event.currentTarget).hasClass("selected");
+    $(".nav-form-item a").each(function(item){
+      $(this).removeClass("selected");
+    });
+    $(".expanded-view").each(function(item){
+      $(this).slideUp(100);
+    });
+    if (!open) {
+      $(event.currentTarget).addClass("selected")
+      $("#picture-expanded-view").slideDown(100);
+    }
+  },
+
+
+  getPictureData: function(formData){
+
+    return formData;
+  },
+
+  createFilePicker: function() {
+
+  },
+
+});
